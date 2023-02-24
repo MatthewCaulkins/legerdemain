f1504ee754327614c17f06c7cb673714933a7ebd
MatthewCaulkins <matt.caulkins@gmail.com>
2023-02-23 21:20:27 -0500

    Halfway through part 2

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const routes = require('./routes/main');
+const secureRoutes = require('./routes/secure');
+
+// setup mongo connection
+const uri = process.env.MONGO_CONNECTION_URL;
+
+mongoose.connect(uri, { useNewUrlParser : true });
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+    process.exit(1);
+});
+
+mongoose.connection.on('connected', function () {
+    console.log('connected to mongo');
+});
 
 // create an instance of an express app
 const app = express();
@@ -14,17 +30,18 @@ app.use(bodyParser.json()); // parse application/json
 
 // main routes
 app.use('/', routes);
-
-// handle errors
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.json({ error : err });
-});
+app.use('/', secureRoutes);
 
 // catch all other routes
 app.use((req, res, next) => {
     res.status(404);
     res.json({ message: '404 - Not Found' });
+});
+
+// handle errors
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error : err });
 });
 
 // have the server start listening on the provided port
